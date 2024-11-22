5dc4ccfab1ee6eef08ba439b75f90107d69514e5
Chukwu Innocent <centkanayo@gmail.com>
2024-11-23 00:14:58 +0100

    Organizing the routes into a mapped array

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
 <?php
 require "functions.php";
-$uri = $_SERVER["REQUEST_URI"]; 
-if($uri === "/"){
-    require "controllers/index.php";
-}else if($uri === "/about"){
-    require "controllers/about.php";
-}else if($uri === "/contact"){
-    require "controllers/contact.php"; 
+$uri = parse_url($_SERVER["REQUEST_URI"])["path"]; 
+$routes = ["/" => "controllers/index.php", "/about" => "controllers/about.php", "/contact" => "controllers/contact.php"];
+if(array_key_exists($uri, $routes)){
+    require $routes[$uri];
 }else{
     echo "<h2>404 not found</h2>";
 }
